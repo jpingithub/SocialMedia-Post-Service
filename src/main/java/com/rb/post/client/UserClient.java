0c840428258b68bafcb5c01427655de1b61a7bb5
jpingithub <nallabothulanagarjun0@gmail.com>
2025-03-18 10:57:00 +0530

    Made it working in local

--- src/main/java/com/rb/post/client/UserClient.java
+++ src/main/java/com/rb/post/client/UserClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "USER-MANAGEMENT-SERVICE", url = "${user-management-service-url}")
+@FeignClient(name = "USER-MANAGEMENT-SERVICE")
 public interface UserClient {
 
     @GetMapping("/api/v1/users/search")
